321cd1fcd81d4c0eb32f35173cda495489f5aaa3
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-11-20 04:05:01 -0500

    Added Javascript Helpers

---
+++ www/scripts/helpers.js
@@ -0,0 +1,17 @@
+﻿// Recieved Pure Javascript HTTPGET code from http://stackoverflow.com/questions/247483/http-get-request-in-javascript
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+};
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
