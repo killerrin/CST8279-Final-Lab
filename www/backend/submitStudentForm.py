b86747f8d5bb980365eea56e0b80c545ef556f1c
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-11-21 02:18:28 -0500

    Added the code to support multiple grades

--- www/backend/submitStudentForm.py
+++ www/backend/submitStudentForm.py
@@ -6,24 +6,27 @@ import sqlite3
 cgitb.enable()
 form = cgi.FieldStorage()
 
+# Get all the Forms
 firstName = form["firstName"].value
 lastName = form["lastName"].value
 courseName = form["courseName"].value
 workType = form["workType"].value
 grades = form["grades"].value
 
-parsedGrade = int(grades)
+# Calculate the Grade
+splitGrades = grades.replace(' ', '').split(',')
+parsedGrade = 0
+for grade in splitGrades:
+    parsedGrade += int(grade)
+parsedGrade = parsedGrade / len(splitGrades)
 
+# Store it to the Database
 con = sqlite3.connect("admin/CST8279.db")
 with con:
     cur = con.cursor()
     cur.execute("INSERT INTO Students (FirstName, LastName, Course, WorkType, Grade) VALUES('{0}', '{1}', '{2}', '{3}', {4})".format(firstName, lastName, courseName, workType, parsedGrade))
 con.close()
 
+# Redirect back to the Form
 print("Location: ../index.html")
 print("Content-type: text/html\n\n")
-#print(firstName)
-#print(lastName)
-#print(courseName)
-#print(workType)
-#print(grades)
