8b47d09f014aa1d87615dd6f715cec8b5b9c8872
Andrew Godfroy <killerrin@users.noreply.github.com>
2016-11-21 23:40:45 -0500

    Added Backup FIles

---
+++ www/backend/backup/getStudentData.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import cgitb
+import cgi
+import sqlite3
+import json
+
+print("Content-type: application/json")
+print("")
+
+students = []
+
+con = sqlite3.connect("admin/CST8279.db")
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Students")   
+    
+    while True:
+        row = cur.fetchone()
+        if row == None:
+            break
+        students.append({"firstName":row[1], "lastName":row[2], "courseName":row[3], "workType":row[4], "grade":row[5]})
+                
+con.close()
+
+print(json.JSONEncoder().encode(students))
